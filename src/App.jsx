170a4b79ddf2d89ae2ec07584fcbd898cc39eef1
David Isea <112912252+davidisea31@users.noreply.github.com>
2024-02-12 20:57:06 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
-    <>
-      <NavBar/>
-
-      <ItemListContainer greeting="Soy un mensaje de prueba" />
-      
-    </>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/category/:id" component={ItemListContainer} />
+          <Route exact path="/item/:id" component={ItemDetailContainer} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
 
+export default App;
